0d162372e307ba9d976e027618f3690db0a93787
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2021-12-09 21:57:49 +0200

    add deleting todo

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,29 +1,54 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { UPDATE_TODO, UPDATE_TODOS } from '../../store/todos/types';
+import {
+  DELETE_TODO_FROM_TODOS,
+  UPDATE_DESCRIPTION,
+  UPDATE_TODO,
+  UPDATE_TODOS,
+} from '../../store/todos/types';
 
-const Todo = () => {
+const TodoItem = ({ id, title, description }) => {
+  const dispatch = useDispatch();
+  const deleteTodo = (id) =>
+    dispatch({ type: DELETE_TODO_FROM_TODOS, payload: id });
+
+  return (
+    <div key={id}>
+      <h4>{title}</h4>
+      <p>{description}</p>
+      <button onClick={() => deleteTodo(id)}>x</button>
+    </div>
+  );
+};
+
+const TodosPage = () => {
   const todo = useSelector((state) => state.todos.currentTodo);
   //TODO: think about naming
   const todos = useSelector((state) => state.todos.todos);
+  const description = useSelector((state) => state.todos.description);
   const dispatch = useDispatch();
 
   const updateTodo = (event) =>
     dispatch({ type: UPDATE_TODO, payload: event.target.value });
 
+  const updateDescription = (event) =>
+    dispatch({ type: UPDATE_DESCRIPTION, payload: event.target.value });
+
   const updateTodos = () => dispatch({ type: UPDATE_TODOS });
+
   return (
     <>
       <form>
         <input value={todo} onChange={updateTodo} placeholder="add todo..." />
+        <input value={description} onChange={updateDescription} />
         <button type="button" onClick={updateTodos}>
           Add todo
         </button>
       </form>
-      {todos.map((todo) => (
-        <div key={todo}>{todo}</div>
+      {todos.map(({ id, title, description }) => (
+        <TodoItem id={id} title={title} description={description} />
       ))}
     </>
   );
 };
 
-export default Todo;
+export default TodosPage;
--- src/store/todos/reducer.js
+++ src/store/todos/reducer.js
@@ -1,15 +1,35 @@
-import { UPDATE_TODO, UPDATE_TODOS } from './types';
+import {
+  UPDATE_TODO,
+  UPDATE_TODOS,
+  UPDATE_DESCRIPTION,
+  DELETE_TODO_FROM_TODOS,
+} from './types';
 
-const todos = (state = { currentTodo: '', todos: [] }, action) => {
+const todos = (
+  state = { currentTodo: '', todos: [], description: '' },
+  action,
+) => {
   switch (action.type) {
     case UPDATE_TODO:
       return { ...state, currentTodo: action.payload };
+    case UPDATE_DESCRIPTION:
+      return { ...state, description: action.payload };
     case UPDATE_TODOS:
+      const newTodo = {
+        title: state.currentTodo,
+        description: state.description,
+        id: state.todos.length + 1,
+      };
       return {
         ...state,
-        todos: [...state.todos, state.currentTodo],
+        todos: [...state.todos, newTodo],
         currentTodo: '',
+        desctiption: '',
       };
+    case DELETE_TODO_FROM_TODOS:
+      const idForDelete = action.payload;
+      const newTodos = state.todos.filter((todo) => todo.id !== idForDelete);
+      return { ...state, todos: newTodos };
     default:
       return state;
   }

d305ba860a9bd2fc20448d936babd3c0263e5a1d
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2021-12-14 20:08:15 +0200

    lifting up state example.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Counter from './components/Counter';
 import LoginForm from './components/LoginForm';
+import RickNMorty from './components/RickNMorty';
 import Todo from './components/Todo';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<LoginForm />} />
         <Route path="/counter" element={<Counter />} />
         <Route path="/todo" element={<Todo />} />
+        <Route path="rick-n-morty" element={<RickNMorty />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/RickNMorty/CharactersList.jsx
@@ -0,0 +1,11 @@
+const CharactersList = ({ characterList = [] }) => {
+  return (
+    <>
+      {characterList.map(({ name }) => (
+        <div>{name}</div>
+      ))}
+    </>
+  );
+};
+
+export default CharactersList;
